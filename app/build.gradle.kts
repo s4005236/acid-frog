5abd48199dcffac2363caf69afc0a2b9e1895f29
ajahn <ajahn@ibes.ag>
2026-02-12 14:29:31 +0100

    fix: temporary fix for hilt with ksp vs agp 9.0.0, needs further fixing, not stable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.google.devtools.ksp)
     alias(libs.plugins.google.dagger.hilt.android)
+
     alias(libs.plugins.kotlin.serialization)
 }
 
